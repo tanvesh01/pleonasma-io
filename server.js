e673936535b2a9c69f977989819ac831d07f7202
Tulsi Prasad <tulsi.prasad50@gmail.com>
2019-12-14 11:50:55 +0530

    :bug: express+react server fix start
    the express server is now server.js and react app is made using create-react-app boilerplate. react needs to fetch the server on port 5000

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+
+const app = express();
+
+app.get('/people', (req, res) => {
+    const people = [
+        {id: 1, firstName: 'Adam', lastName: 'Apple'},
+        {id: 2, firstName: 'Adam', lastName: 'Apple'},
+        {id: 3, firstName: 'Adam', lastName: 'Apple'}
+    ];
+
+    return res.json(people);
+});
+
+const port = 5000;
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
